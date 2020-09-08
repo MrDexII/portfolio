7890f64ccf91707c996338fa421cd6f22c79579f
MrDexII <free.andrew.a@gmail.com>
2020-09-08 11:36:51 +0200

    mobile nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,24 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from '../styles/Nav-style.module.css'
 
+function NormalToggleMenu({ style, handleClick }) {
+    const { menuStyle, navStyle } = style
+    return (
+        <nav className={navStyle}>
+            <ul className={menuStyle}>
+                <li onClick={handleClick}><a href='#about' >O mnie</a></li>
+                <li onClick={handleClick}><a href='#projects'>Projekty</a></li>
+                <li onClick={handleClick}><a href='#contact'>Kontakt</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+function MobileToggleMenu({ handleClick }) {
+    return (
+        <NormalToggleMenu handleClick={handleClick} style={{ menuStyle: styles.mobileMenu, navStyle: styles.mobileNav }} />
+    )
+}
+
 function Nav({ isMobile }) {
+    const [showMobileView, setShowMobileView] = useState(false)
+
+    const handleClick = () => {
+        if (isMobile) {
+            setShowMobileView(prev => { return !prev })
+        }
+    }
+
     return (
         <div className={isMobile ? styles.mobileContainer : styles.container} >
-            {isMobile ?
-                <button className={styles.menuButton}>
-                    <span>
-                        toggle menu
-                </span>
-                </button> :
-                <nav >
-                    <ul className={styles.navbar}>
-                        <li><a href='#about'>O mnie</a></li>
-                        <li><a href='#projects'>Projekty</a></li>
-                        <li><a href='#contact'>Kontakt</a></li>
-                    </ul>
-                </nav>
+            {
+                isMobile ?
+                    <button className={`${styles.menuButton} ${showMobileView ? styles.isActive : ""}`} onClick={handleClick}>
+                        <span>
+                            toggle menu
+                        </span>
+                    </button> :
+                    <NormalToggleMenu style={{ menuStyle: styles.navbar }} />
             }
-
+            {
+                showMobileView && isMobile ?
+                    <MobileToggleMenu handleClick={handleClick} /> :
+                    ""
+            }
         </div>
     )
 }

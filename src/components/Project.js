febdcd0f7a2ab7a3dfd9e41b39552c216b00b004
unknown <free.andrew.a@gmail.com>
2022-12-01 18:59:44 +0100

    more button

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,17 +1,37 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "../styles/Project-style.module.css";
 
 function Project({ svg, name, image, description, buttons, id }) {
   const [flipFace, setFlipFace] = useState(false);
+  const [moreInfo, setMoreInfo] = useState(false);
 
   const handleMouseEnter = () => {
     setFlipFace((prev) => !prev);
   };
 
+  const onClickMoreEvent = () => {
+    setMoreInfo((prev) => !prev);
+  };
+
+  useEffect(() => {
+    if (moreInfo === true) {
+      setFlipFace(true);
+    } else {
+      setFlipFace(false);
+    }
+  }, [moreInfo]);
+
   const More = () => {
     return (
       <>
-        <div className={styles.imageContainer}>{image}</div>
+        <div className={styles.imageContainer}>
+          {image}
+          <div>
+            <button className={styles.maxIcone} onClick={onClickMoreEvent}>
+              More
+            </button>
+          </div>
+        </div>
         <div className={styles.description}>
           <p>{description}</p>
           <div className={styles.buttonsContainer}>
@@ -29,9 +49,12 @@ function Project({ svg, name, image, description, buttons, id }) {
   return (
     <div
       id={name}
-      className={`${styles.container} ${flipFace ? styles.flip : ""}`}
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseEnter}
+      className={`${styles.container} ${flipFace ? styles.flip : ""} ${
+        moreInfo ? styles.more : ""
+      }`}
+      // onMouseEnter={moreInfo ? () => {} : handleMouseEnter}
+      // onMouseLeave={moreInfo ? () => {} : handleMouseEnter}
+      onMouseOver={moreInfo ? () => {} : handleMouseEnter}
     >
       {flipFace ? <More /> : svg}
     </div>
